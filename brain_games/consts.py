7d5873025896ec006cd20719488eceac76e7f966
Daria_Sotnikova <dashachizh@gmail.com>
2025-11-07 15:12:55 +0300

    Update files consts.py, engine.py, pyproject.toml

--- brain_games/consts.py
+++ brain_games/consts.py
@@ -9,3 +9,5 @@ MATH_SIGNS = ('+', '-', '*')
 
 EVEN_INSTRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 CALC_INSTRUCTION = 'What is the result of the expression?'
+GCD_INSTRUCTION = 'Find the greatest common divisor of given numbers.'
+
