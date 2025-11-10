208fa54be3770f4618e639401c0448926d0c09b3
Daria_Sotnikova <dashachizh@gmail.com>
2025-11-10 15:43:02 +0300

    Update logic gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -7,17 +7,11 @@ def get_nums_and_answer():
     num_1 = get_random_num()
     num_2 = get_random_num()
     a, b = num_1, num_2
-    gcd_value = ''
-  
-    if b == 0 or a == b:
-        gcd_value = a
-    else:
-        remainder = a % b
-        while remainder != 0:
-            remainder = a % b
-            a = b
-            b = remainder
-            gcd_value = a
+
+    while b != 0:
+        a = b 
+        b = a % b 
+    gcd_value = a
 
     return f'{num_1} {num_2}', str(gcd_value)
 
