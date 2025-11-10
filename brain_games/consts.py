7ded49662b65040cc3d880c6c0e1177c5fda4c04
Daria_Sotnikova <dashachizh@gmail.com>
2025-11-10 15:25:42 +0300

    Update consts.py

--- brain_games/consts.py
+++ brain_games/consts.py
@@ -8,7 +8,6 @@ MAX_LENGTH = 11
 MATH_SIGNS = ('+', '-', '*')
 
 # games instructions
-
 EVEN_INSTRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 CALC_INSTRUCTION = 'What is the result of the expression?'
 GCD_INSTRUCTION = 'Find the greatest common divisor of given numbers.'
